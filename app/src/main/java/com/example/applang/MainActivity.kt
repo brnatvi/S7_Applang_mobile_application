7a69e15b8cbbe3cd263c44dcecc638887fe18c3e
BRAGINA natalia <nata.br@gmail.com>
2022-11-17 15:12:05 +0100

    first layouts, dimens, strings, colors values apdated, binding in main activity

--- app/src/main/java/com/example/applang/MainActivity.kt
+++ app/src/main/java/com/example/applang/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.applang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.applang.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    fun traduire(view: View){
+
+    }
+
+    fun chercher(view: View){
+
+    }
+
 }
