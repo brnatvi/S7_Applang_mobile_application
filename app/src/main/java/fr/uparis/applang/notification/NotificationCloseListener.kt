efb24c8d159d26c2b65a0ed13018514bb579ab3d
raveneau <emilien.raveneau@gmail.com>
2023-01-03 17:25:09 +0100

    clean code

--- app/src/main/java/fr/uparis/applang/notification/NotificationCloseListener.kt
+++ app/src/main/java/fr/uparis/applang/notification/NotificationCloseListener.kt
@@ -4,19 +4,12 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import fr.uparis.applang.model.LanguageApplication
-import fr.uparis.applang.navigation.OptionsMenuActivity
 
 class NotificationCloseListener: BroadcastReceiver() {
-    private val dao by lazy { LanguageApplication().database.langDAO() }
-    private val oma = OptionsMenuActivity()
-
 
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.d("NOTIFICATIONS","Update word")
         if(intent!=null && intent.extras != null && context!=null){
-//            val url: String = intent!!.getStringExtra("URL")!!
-            Log.d("NOTIFICATIONS","Update word 2")
             var intentUW = Intent(context, UpdateWordService::class.java ).apply {
                 action = "updateWord"
             }
